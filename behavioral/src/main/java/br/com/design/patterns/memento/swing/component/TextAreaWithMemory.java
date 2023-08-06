6c33d8ca667a3f8deaf803979b8106e0a8173328
Vinicius Telles <telles.code@gmail.com>
2023-08-06 15:01:13 -0300

    Add memento design pattern

---
+++ behavioral/src/main/java/br/com/design/patterns/memento/swing/component/TextAreaWithMemory.java
@@ -0,0 +1,35 @@
+package br.com.design.patterns.memento.swing.component;
+
+
+import br.com.design.patterns.memento.swing.memory.Memento;
+
+import javax.swing.*;
+
+public class TextAreaWithMemory extends JTextArea {
+	private static final long serialVersionUID = 2393587108160522742L;
+	
+	public TextAreaWithMemory(int rows, int columns) {
+		super(rows, columns);
+	}
+
+	public TextAreaMemento save() {
+		return new TextAreaMemento(getText());
+	}
+	
+	public void restore(TextAreaMemento memento) {
+		setText(memento.getState());
+	}
+	
+	public static class TextAreaMemento implements Memento {
+		private String text;
+
+		public TextAreaMemento(String text) {
+			this.text = text;
+		}
+
+		private String getState() {
+			return text;
+		}
+	}
+
+}
---
+++ behavioral/src/main/java/br/com/design/patterns/memento/swing/memory/Memento.java
@@ -0,0 +1,5 @@
+package br.com.design.patterns.memento.swing.memory;
+
+public interface Memento {
+
+}
